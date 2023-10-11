af218cacafc36d82788863ccc3679ebc3faea578
aleeshan123 <113351859+aleeshan813@users.noreply.github.com>
2023-10-11 15:07:56 +0530

    Update for mobile game

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -9,11 +9,18 @@ public class Hit : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // Check if the collision is with the vehicles GameObject
-        if (collision.gameObject.CompareTag("Vehicles") || collision.gameObject.CompareTag("Pedestrians"))
-        {            // Destroy the player GameObject
+        if (collision.gameObject.CompareTag("Vehicles"))
+        {            
             ExitMenu.SetActive(true);
             Time.timeScale = 0f;
         }
+
+        else if(collision.gameObject.CompareTag("Pedistrains"))
+        {
+            ExitMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
         else
         {
             ExitMenu.SetActive(false);
